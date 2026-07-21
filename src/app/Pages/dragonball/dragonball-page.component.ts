456d4e8f20ef50b1a8675daf205f7325c0deded7
Juan Toro <juanmanueltoromarin@gmail.com>
2026-07-20 19:29:57 -0500

    Se agrega un sencillo formulario

---
+++ src/app/Pages/dragonball/dragonball-page.component.ts
@@ -0,0 +1,10 @@
+
+import { Component } from "@angular/core";
+
+@Component({
+
+    templateUrl: './dragonball-page.component.html',
+   
+})
+export class dragonballPageComponent{}
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CounterPageComponent } from './Pages/counter/counter-page.component';
 import {HeroPageComponent} from './Pages/hero/hero-page.component';
+import { dragonballPageComponent } from './Pages/dragonball/dragonball-page.component';
 export const routes: Routes = [
     {
         path: '',
@@ -9,5 +10,13 @@ export const routes: Routes = [
     {
         path: 'hero',
         component: HeroPageComponent, 
-    }
+    },
+    {
+       path: 'dragonball',
+       component: dragonballPageComponent,
+    },
+    {
+        path: '**',
+        redirectTo: '', 
+    },
 ];
