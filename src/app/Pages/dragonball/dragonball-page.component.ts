565948812c8041b078e088833b362fbdb6ccf4db
Juan Toro <juanmanueltoromarin@gmail.com>
2026-07-21 20:20:33 -0500

    se modifica algo peqeuño

--- src/app/Pages/dragonball/dragonball-page.component.ts
+++ src/app/Pages/dragonball/dragonball-page.component.ts
@@ -1,10 +1,26 @@
 
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
+
+
+interface Characters{
+    id: number;
+    name: string;
+    power: number;
+}
 
 @Component({
 
     templateUrl: './dragonball-page.component.html',
    
 })
-export class dragonballPageComponent{}
+export class dragonballPageComponent{
+
+characters = signal < Characters[]>([
+ {id: 1, name: 'Goku', power: 10012},
+ {id: 2, name: 'El chavo', power: 9002},
+  {id: 3, name: 'Vegeta', power: 9023},
+]);
+
+
+}
 
